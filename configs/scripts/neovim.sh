a241c0c1a2a3b4abea651ea8b8176e75b782bac2
Hadi Chokr <158838697+silverhadch@users.noreply.github.com>
2024-08-28 10:42:34 +0200

    Update neovim.sh

--- configs/scripts/neovim.sh
+++ configs/scripts/neovim.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-echo "Not Finished for Gentoo"
-#Build prerequisites
-#sudo emerge --ask ninja-build gettext cmake unzip curl
-
-# clone
-#git clone https://github.com/neovim/neovim
-#cd neovim
-#git checkout stable
-#make CMAKE_BUILD_TYPE=RelWithDebInfo
-#cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb
+sudo emerge --ask ninja-build gettext cmake unzip curl
+sudo emerge --ask app-editors/neovim
 
